181eb0d99ac8457e692cc22a3be79923edc683f5
jbinaux <37798129+jbinaux@users.noreply.github.com>
2018-11-12 16:30:38 +0100

    initialisation

---
+++ src/main/java/co/simplon/BoiteAJeu/CertificationBoiteAJeuApplication.java
@@ -0,0 +1,12 @@
+package co.simplon.BoiteAJeu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CertificationBoiteAJeuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CertificationBoiteAJeuApplication.class, args);
+	}
+}
