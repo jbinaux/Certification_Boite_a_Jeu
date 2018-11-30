9461714e0ff333170dcf65b5e474737d04b67b60
jbinaux <37798129+jbinaux@users.noreply.github.com>
2018-11-30 17:08:26 +0100

    footer, mock review to display

--- src/main/webapp/WEB-INF/Boite-a-jeu-front/src/app/app-routing.module.ts
+++ src/main/webapp/WEB-INF/Boite-a-jeu-front/src/app/app-routing.module.ts
@@ -1,8 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { SignInRegisterMasterComponent} from './sign-in-register-master/sign-in-register-master.component';
+import { HomepageComponent } from './homepage/homepage.component';
 
 const routes: Routes = [
+  { path: '', component: HomepageComponent },
   { path: 'sign-in', component: SignInRegisterMasterComponent }
 ];
 
