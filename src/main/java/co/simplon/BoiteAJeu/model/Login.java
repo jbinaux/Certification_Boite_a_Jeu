62fb0f9171f16e252710510d1885794cf707f6f2
jbinaux <37798129+jbinaux@users.noreply.github.com>
2018-12-10 10:00:13 +0100

    separate user from login

---
+++ src/main/java/co/simplon/BoiteAJeu/model/Login.java
@@ -0,0 +1,53 @@
+package co.simplon.BoiteAJeu.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToOne;
+
+@Entity
+public class Login {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private long id;
+	
+	private String email;
+	private String password;
+	
+	@OneToOne(mappedBy = "login")
+	private User user;
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public User getUser() {
+		return user;
+	}
+
+	public void setUser(User user) {
+		this.user = user;
+	}
+}
--- src/main/java/co/simplon/BoiteAJeu/model/User.java
+++ src/main/java/co/simplon/BoiteAJeu/model/User.java
@@ -18,8 +18,6 @@ public class User {
 	private long id;
 	
 	private String username;
-	private String email;
-	private String password;
 	private boolean admin;
 	
 	@JsonIgnore
@@ -40,22 +38,6 @@ public class User {
 
 	public void setUsername(String username) {
 		this.username = username;
-	}
-
-	public String getEmail() {
-		return email;
-	}
-
-	public void setEmail(String email) {
-		this.email = email;
-	}
-
-	public String getPassword() {
-		return password;
-	}
-
-	public void setPassword(String password) {
-		this.password = password;
 	}
 
 	public boolean isAdmin() {
